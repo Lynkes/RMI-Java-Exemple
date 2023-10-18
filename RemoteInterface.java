4d0cd0600f54d8566d36d5691b66dd7d26e9ada2
Pedro Guilherme Gabriel Maurer <48015282+Lynkes@users.noreply.github.com>
2023-10-18 11:13:47 -0300

    Delete RemoteInterface.java

--- RemoteInterface.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface RemoteInterface extends Remote {
-    int increment(int number) throws RemoteException;
-    int decrement(int number) throws RemoteException;
-    int countCharacters(String text) throws RemoteException;
-    float addFloats(float num1, float num2) throws RemoteException;
-}
